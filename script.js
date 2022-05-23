0bf8b7354301ffd5a79c92c57f52b07c36989395
Christian Gullord <gullord.christian@gmail.com>
2022-05-23 16:49:55 -0500

    Button Clicker

---
+++ script.js
@@ -0,0 +1,9 @@
+function signOut(element) {
+    element.innerText = "Sign Out";
+}
+function hide(element) {
+    element.remove();
+}
+function likesNinja(element) {
+    console.log("Ninja was liked", element);
+}
